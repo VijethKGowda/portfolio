1b8ac0df43b427aaae0819f08a80d3bb6a79bb5e
VijethK <vijethk@Vijeths-MacBook-Pro.local>
2022-09-05 19:07:24 +0530

    resume added

--- pages/Components/Navbar.js
+++ pages/Components/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = ({ setTheme, theme }) => {
       <div className='flex justify-between text-gray-800 dark:text-slate-200 py-6 md:py-9 lg:py-11'>
         <div className='font-light text-2xl'>VJ<sup>th</sup></div>
         <div className='flex gap-4 md:gap-6 lg:gap-8 items-center sm:text-sm md:text-base lg:text-lg'>
-          <button className='font-light'>Resume</button>
-          <a className='cursor-pointer font-light'>Contact</a>
+          <a href='https://docs.google.com/document/d/1AglclbhNABpTcusWhRZ6M0AgVQNROTS8u6nOKkG7ws4/edit?usp=sharing' target="_blank" className='font-light' rel="noreferrer">Resume</a>
+          {/* <a className='cursor-pointer font-light'>Contact</a> */}
           {
             theme === 'light' ?
               <button onClick={() => { setTheme('dark') }} className=''>
